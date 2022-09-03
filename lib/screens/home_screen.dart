2fb56dd2c3ec3b255bc4f7fa08e76eadb940a339
Amber Jain <jainanuamber@gmail.com>
2022-09-04 00:02:38 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,7 +18,53 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(
-        children: [],
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    "assets/images/profile_image.png",
+                    fit: BoxFit.cover,
+                    height: 50,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          "Welcome Back",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        SizedBox(height: 2.5),
+                        Text(
+                          "Amber Jain",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 100),
+                    const Icon(
+                      FontAwesomeIcons.moneyBillTrendUp,
+                      color: Colors.grey,
+                      size: 25,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Divider(thickness: 0.5)
+        ],
       )),
     );
   }
