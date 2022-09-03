73fe77bae5c5c2c88c3bc620f9b02495def72bee
Amber Jain <jainanuamber@gmail.com>
2022-09-04 00:27:15 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,57 +17,68 @@ class _HomeScreenState extends State<HomeScreen> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(top: 25),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: Image.asset(
-                    "assets/images/profile_image.png",
-                    fit: BoxFit.cover,
-                    height: 50,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
-                          "Welcome Back",
-                          style: TextStyle(fontSize: 15),
-                        ),
-                        SizedBox(height: 2.5),
-                        Text(
-                          "Amber Jain",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.asset(
+                      "assets/images/profile_image.png",
+                      fit: BoxFit.cover,
+                      height: 50,
                     ),
-                    const SizedBox(width: 100),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        FontAwesomeIcons.bars,
-                        color: Colors.grey,
-                        size: 30,
+                  ),
+                  const SizedBox(width: 10),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Welcome Back",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          SizedBox(height: 2.5),
+                          Text(
+                            "Amber Jain",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 100),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          FontAwesomeIcons.bars,
+                          color: Colors.grey,
+                          size: 30,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 10),
-          const Divider(thickness: 0.5)
-        ],
+            const SizedBox(height: 10),
+            const Divider(thickness: 0.5),
+            const SizedBox(height: 25),
+            Container(
+              alignment: Alignment.bottomLeft,
+              padding: const EdgeInsets.only(left: 25, right: 150),
+              child: const Text(
+                "Complete your KYC now 😡",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
